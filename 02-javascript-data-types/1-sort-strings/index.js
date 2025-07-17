ddbc2b31e2e681d762cf57ccdde2b8c31cd4ac13
iraczyu <ira_czyu@mail.ru>
2025-07-17 12:28:50 +0300

    first and thecond tasks

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,11 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    const newArr = [...arr];
+    if (param === 'asc') {
+        return newArr.sort( (a, b) =>a.localeCompare(b,["ru", "en"],{ caseFirst: "upper" }) );
+    } else if (param === 'desc') {
+        return newArr.sort( (a, b) => b.localeCompare(a,["ru", "en"],{ caseFirst: "upper" }) );
+    }
 }
+
