956dca98fe3caae8048f34912d59ca8e188266d5
iraczyu <ira_czyu@mail.ru>
2025-07-22 23:15:32 +0300

    task 3.1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const newArr = path.split('.');
 
+    return function(obj) {
+        for (const key of newArr) {
+            if (obj === undefined || obj === null || !obj.hasOwnProperty([key])) {
+                return undefined;
+            }
+            obj = obj[key];
+        }
+    return obj;
+  }
 }
